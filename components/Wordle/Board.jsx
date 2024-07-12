4487a83203e4fceb013300432ec92afbe5f4b35b
Denver <csdedwards@gmail.com>
2024-07-12 14:39:36 -0400

    fix

--- components/Wordle/Board.jsx
+++ components/Wordle/Board.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import Link from "next/link";
-import { words } from "@/utils/words";
+import { fiveLetterWords as words } from "@/utils/words";
+import Button from "@/components/Button";
 
 export default function Wordle() {
   const [letters, setLetters] = useState([]);
@@ -15,6 +16,20 @@ export default function Wordle() {
     setLetters([...Array(26)].map((_, i) => String.fromCharCode(i + 65)));
     resetGame();
   }, []);
+
+  useEffect(() => {
+    if (wrongTries === 6) {
+      setUserInput(selectedWord.split("").map((char) => char.toUpperCase()));
+      toast.error(
+        `Too many tries. You lose! The word was: ${selectedWord.toUpperCase()}`,
+        {
+          toastId: "notice-bad",
+          className: "custom-toast-error",
+        }
+      );
+      setGameOver(true);
+    }
+  }, [wrongTries]);
 
   function checkLetter(letter) {
     if (wrongTries < 6 && !gameOver) {
@@ -40,7 +55,7 @@ export default function Wordle() {
   function resetGame() {
     const newWord = selectRandomWord();
     setSelectedWord(newWord);
-    setUserInput(Array(newWord.length).fill("_"));
+    setUserInput(Array(newWord.length).fill(" "));
     setWrongTries(0);
     setIncorrectLetter([]);
     setGameOver(false);
@@ -60,6 +75,45 @@ export default function Wordle() {
       >
         {"<"} Back
       </Link>
+
+      <div className="flex space-x-2 flex-wrap justify-center">
+        {userInput.map((char, index) => (
+          <div
+            key={index}
+            className="w-10 h-10 flex items-center justify-center text-center text-3xl rounded-md bg-white shadow"
+          >
+            {char}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-6 sm:grid-cols-7 gap-2 mt-4">
+        {letters.map((letter) => (
+          <button
+            className={`w-12 sm:w-20 h-12 sm:h-20 flex items-center justify-center text-center text-2xl sm:text-3xl border rounded-2xl drop-shadow-md ${
+              incorrectLetter.includes(letter)
+                ? "bg-red-400 text-white cursor-default border-none drop-shadow-xl"
+                : "bg-white hover:bg-gray-200 hover:text-gray-900 transition"
+            }`}
+            key={letter}
+            onClick={() => checkLetter(letter)}
+            disabled={wrongTries === 6 || gameOver}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+
+      <div className="absolute top-10 right-10 flex flex-col items-end space-y-2">
+        <div className="text-lg text-slate-700">Mistakes: {wrongTries}</div>
+        <Button
+          passFunc={resetGame}
+          text="Reset"
+          customCSS={"bg-rose-600 hover:bg-rose-700 "}
+        />
+      </div>
+
+      <div className="flex justify-center mt-4"></div>
     </div>
   );
 }
